259d9df7b49ba28dfd1ae4fa6fe95cb250ec073c
Alexei Bushuev <alexeib.2018@gmail.com>
2018-10-27 09:55:34 +0300

    fixed tests

--- import/tests.py
+++ import/tests.py
@@ -1,17 +1,18 @@
 from django.test import TestCase
-from .views import Record
+from .views import FileRecord
 from .models import Disk, Folder
 
 
 class ImportTestCase(TestCase):
+    disk_filename = 'disk_filename.fbd'
     disk_name = 'DiskName'
     disk_time = '2018-01-01 00:00:00'
     disk = None
 
     def setUp(self):
-        disk = Record.create_disk(self.disk_name, self.disk_time)
+        disk = FileRecord.create_disk(self.disk_filename, self.disk_name, self.disk_time)
         assert disk == True
-        self.disk = Record.get_disk(self.disk_name, self.disk_time)
+        self.disk = FileRecord.get_disk(self.disk_name, self.disk_time)
         assert self.disk.name == self.disk_name
 
     def test_create_disk(self):
@@ -21,15 +22,15 @@ class ImportTestCase(TestCase):
     def test_recreate_disk(self):
         disk1 = Disk.objects.get(id=1)
         assert disk1.name == self.disk_name
-        disk = Record.create_disk(self.disk_name, self.disk_time)
+        disk = FileRecord.create_disk(self.disk_filename, self.disk_name, self.disk_time)
         assert disk == False
 
     def test_create_empty_folder(self):
-        Record.get_folder(self.disk, [])
+        FileRecord.get_folder(self.disk, [])
         folders = Folder.objects.all()
         assert len(folders) == 0
 
     def test_create_folder(self):
-        Record.get_folder(self.disk, ['folder'])
+        FileRecord.get_folder(self.disk, ['folder'])
         folders = Folder.objects.all()
         assert len(folders) == 1
